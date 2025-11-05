a8199be367eadc6d57e6f361966d22929e51b84f
Abel <abel.hubena.developer@gmail.com>
2025-11-05 21:55:37 +0000

    chore: Bump version to 1.0.3 and fix CLI version display

--- src/cli.ts
+++ src/cli.ts
@@ -12,7 +12,7 @@ const program = new Command();
 program
   .name('zembil')
   .description('Offline Package & Docs Cache for developers with unreliable internet')
-  .version('1.0.0');
+  .version('1.0.3');
 
 // Initialize command
 program
